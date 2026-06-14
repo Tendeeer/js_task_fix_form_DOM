59045d81ff8cb6fa251a0e0e7236a011cce6078f
Tendeeer <leifisisidj747@gmail.com>
2026-06-14 19:24:12 +0300

    error fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const input = document.querySelectorAll('input');
+const input = document.querySelectorAll('form input');
 
 input.forEach((item) => {
   const createLabel = document.createElement('label');
